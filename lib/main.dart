fb8a2dce386d8546cb5c7ff98f4704e2439ba340
alibabakutty <tallysriram007@gmail.com>
2025-02-05 11:37:27 +0530

    after combining employer and employee login page into single login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:database_app/firebase_options.dart';
-import 'package:database_app/screens/employee_login_register.dart';
-import 'package:database_app/screens/employer_login_register.dart';
 import 'package:database_app/screens/home_page.dart';
+import 'package:database_app/screens/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:database_app/widget_tree.dart';
@@ -26,9 +25,9 @@ class DatabaseApp extends StatelessWidget {
       routes: {
         '/': (context) => const WidgetTree(), // Entry point of the app
         '/employer_login': (context) =>
-            const EmployerLoginRegister(), // Employer Login page
+            const LoginPage(isEmployer: true), // Employer Login page
         '/employee_login': (context) =>
-            EmployeeLoginRegister(), // Employee Login page route
+            const LoginPage(isEmployer: false), // Employee Login page route
         '/home': (context) => HomePage(), // Home page route
       },
     );
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,57 +1,63 @@
-import 'package:database_app/models/user_model.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:database_app/authentication/auth.dart';
+import 'package:database_app/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:database_app/services/firebase_service.dart';
 
-class EmployeeLoginRegister extends StatefulWidget {
-  const EmployeeLoginRegister({super.key});
+class LoginPage extends StatefulWidget {
+  final bool isEmployer; // Determines if the user is an employer or employee
+  const LoginPage({super.key, required this.isEmployer});
 
   @override
-  State<EmployeeLoginRegister> createState() => _EmployeeLoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _otpController = TextEditingController();
   String? errorMessage = '';
-  bool isLogin = true; // Toggle between login and register
-  bool useEmail = true; // Toggle between email and phone number
+  bool isLogin = true; // toggle between login and register
+  bool useEmail = false; // toggle between email and phone number
   String? verificationId;
   bool isOtpSent = false;
 
-  /// Handles email authentication
-  Future<void> signInOrRegisterWithEmail() async {
+  // Handles email authentication
+  Future<void> siginInOrRegisterWithEmail() async {
     try {
       if (isLogin) {
-        // Employee login
+        // Login
         await Auth().signInWithEmailAndPassword(
           email: _emailController.text.trim(),
           password: _passwordController.text.trim(),
         );
       } else {
-        // Employee register
+        // Register
         UserCredential userCredential =
             await Auth().createUserWithEmailAndPassword(
           email: _emailController.text.trim(),
           password: _passwordController.text.trim(),
         );
 
+        // Add user data to firestore
         UserModel newUser = UserModel(
           userNo: userCredential.user!.uid,
           userName: _userNameController.text.trim(),
           userEmail: _emailController.text.trim(),
           userPassword: _passwordController.text.trim(),
           phoneNo: _phoneController.text.trim(),
-          isEmployer: false,
+          isEmployer: widget.isEmployer, // use the employer flag
           createdAt: DateTime.now(),
         );
         await Auth().addUser(newUser);
       }
       if (mounted) {
-        Navigator.of(context).pushReplacementNamed('/home');
+        Navigator.of(context).pushReplacementNamed(
+          '/home',
+          arguments: widget.isEmployer,
+        ); // pass the isemployer flag
       }
     } on FirebaseAuthException catch (e) {
       setState(() {
@@ -60,7 +66,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
     }
   }
 
-  /// Initiates phone authentication (OTP verification)
+  // Initates phone authentication(OTP verification)
   Future<void> sendOtp() async {
     try {
       await Auth().verifyPhoneNo(
@@ -74,10 +80,12 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
         onVerificationCompleted: (PhoneAuthCredential credential) async {
           UserCredential userCredential =
               await Auth().sigInWithCredential(credential);
-          await saveEmployeeToFireStore(userCredential);
+          await saveUserToFirestore(userCredential);
           if (mounted) {
-            Navigator.of(context)
-                .pushReplacementNamed('/home', arguments: false);
+            Navigator.of(context).pushReplacementNamed(
+              '/home',
+              arguments: widget.isEmployer, // pass the isemployer flag
+            );
           }
         },
         onVerificationFailed: (FirebaseAuthException e) {
@@ -98,8 +106,8 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
     }
   }
 
-  /// Handles OTP verification
-  Future<void> verifyOtp() async {
+  // Handles OTP Verification
+  Future<void> verifiyOtp() async {
     try {
       if (verificationId != null) {
         PhoneAuthCredential credential = PhoneAuthProvider.credential(
@@ -108,14 +116,17 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
         );
         UserCredential userCredential =
             await Auth().sigInWithCredential(credential);
-        await saveEmployeeToFireStore(userCredential);
+        await saveUserToFirestore(userCredential);
         if (mounted) {
-          Navigator.of(context).pushReplacementNamed('/home', arguments: false);
+          Navigator.of(context).pushReplacementNamed(
+            '/home',
+            arguments: widget.isEmployer, // pass the isemployer flag
+          );
+        } else {
+          setState(() {
+            errorMessage = 'Invalid OTP';
+          });
         }
-      } else {
-        setState(() {
-          errorMessage = 'Invalid OTP';
-        });
       }
     } catch (e) {
       setState(() {
@@ -124,18 +135,19 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
     }
   }
 
-  Future<void> saveEmployeeToFireStore(UserCredential userCredential) async {
+  // saves user data to firestore
+  Future<void> saveUserToFirestore(UserCredential userCredential) async {
     if (userCredential.user != null) {
       bool userExists =
           await Auth().checkIfUserExists(userCredential.user!.uid);
       if (!userExists) {
         UserModel newUser = UserModel(
           userNo: userCredential.user!.uid,
-          userName: "",
-          userEmail: "",
-          userPassword: "",
+          userName: _userNameController.text.trim(),
+          userEmail: _emailController.text.trim(),
+          userPassword: _passwordController.text.trim(),
           phoneNo: _phoneController.text.trim(),
-          isEmployer: false,
+          isEmployer: widget.isEmployer,
           createdAt: DateTime.now(),
         );
         await Auth().addUser(newUser);
@@ -147,7 +159,11 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(isLogin ? 'Employee Login' : 'Employee Register'),
+        title: Text(
+          widget.isEmployer
+              ? (isLogin ? 'Employer Login' : 'Employer Register')
+              : (isLogin ? 'Employee Login' : 'Employee Register'),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
         elevation: 0,
@@ -158,7 +174,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Toggle between Email and Phone Authentication
+              // Toggle between email and phone authentication
               Center(
                 child: ToggleButtons(
                   isSelected: [useEmail, !useEmail],
@@ -171,7 +187,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                   selectedColor: Colors.white,
                   fillColor: Colors.blueAccent,
                   constraints: const BoxConstraints(
-                    minHeight: 40,
+                    minHeight: 30,
                     minWidth: 120,
                   ),
                   children: const [
@@ -179,9 +195,12 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                       padding: EdgeInsets.symmetric(horizontal: 16.0),
                       child: Row(
                         children: [
-                          Icon(Icons.email, color: Colors.indigoAccent),
+                          Icon(
+                            Icons.email,
+                            color: Colors.indigoAccent,
+                          ),
                           SizedBox(width: 8),
-                          Text("Email"),
+                          Text('Email'),
                         ],
                       ),
                     ),
@@ -189,9 +208,12 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                       padding: EdgeInsets.symmetric(horizontal: 16.0),
                       child: Row(
                         children: [
-                          Icon(Icons.phone, color: Colors.indigoAccent),
+                          Icon(
+                            Icons.phone,
+                            color: Colors.indigoAccent,
+                          ),
                           SizedBox(width: 8),
-                          Text("Phone"),
+                          Text('Phone No'),
                         ],
                       ),
                     ),
@@ -224,7 +246,10 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                   controller: _emailController,
                   decoration: const InputDecoration(
                     labelText: 'Email',
-                    prefixIcon: Icon(Icons.email, color: Colors.blueAccent),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.blueAccent,
+                    ),
                     border: OutlineInputBorder(),
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.blueAccent),
@@ -235,9 +260,12 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                 const SizedBox(height: 16),
                 TextField(
                   controller: _passwordController,
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock, color: Colors.blueAccent),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.blueAccent,
+                    ),
                     border: OutlineInputBorder(),
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.blueAccent),
@@ -255,7 +283,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: signInOrRegisterWithEmail,
+                    onPressed: siginInOrRegisterWithEmail,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blueAccent,
                       padding: const EdgeInsets.symmetric(vertical: 16),
@@ -278,7 +306,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                     onPressed: () => setState(() => isLogin = !isLogin),
                     child: Text(
                       isLogin
-                          ? 'Need an account? Register'
+                          ? 'Need an Account? Register'
                           : 'Already have an account? Login',
                       style: const TextStyle(color: Colors.blueAccent),
                     ),
@@ -288,12 +316,14 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                 // Phone Number Authentication
                 TextField(
                   controller: _phoneController,
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     labelText: 'Enter Phone Number',
-                    prefixIcon: Icon(Icons.phone, color: Colors.blueAccent),
+                    prefixIcon: Icon(
+                      Icons.phone,
+                      color: Colors.blueAccent,
+                    ),
                     prefixText: '+91-',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
+                    border: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.blueAccent),
                     ),
                   ),
@@ -303,9 +333,12 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                 if (isOtpSent) ...[
                   TextField(
                     controller: _otpController,
-                    decoration: const InputDecoration(
-                      labelText: 'OTP',
-                      prefixIcon: Icon(Icons.lock, color: Colors.blueAccent),
+                    decoration: InputDecoration(
+                      labelText: 'Enter OTP',
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.blueAccent,
+                      ),
                       border: OutlineInputBorder(),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.blueAccent),
@@ -316,13 +349,13 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                   if (errorMessage!.isNotEmpty)
                     Text(
                       errorMessage!,
-                      style: const TextStyle(color: Colors.red),
+                      style: TextStyle(color: Colors.redAccent),
                     ),
                   const SizedBox(height: 20),
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
-                      onPressed: verifyOtp,
+                      onPressed: verifiyOtp,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blueAccent,
                         padding: const EdgeInsets.symmetric(vertical: 16),
@@ -350,7 +383,7 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
+                      ), 
                       child: const Text(
                         'Send OTP',
                         style: TextStyle(
@@ -360,8 +393,8 @@ class _EmployeeLoginPageState extends State<EmployeeLoginRegister> {
                       ),
                     ),
                   ),
-                ],
-              ],
+                ]
+              ]
             ],
           ),
         ),
